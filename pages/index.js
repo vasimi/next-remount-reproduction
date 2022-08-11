b88a20188d9eee5ce8424681258d12a1a33036a3
Michal Vašíček <michal.vasicek@firma.seznam.cz>
2022-08-11 12:59:57 +0200

    add mount&unmount logs to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import { useEffect } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  useEffect(() => {
+    console.log('mount');
+    return () => {
+      console.log('unmount');
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
